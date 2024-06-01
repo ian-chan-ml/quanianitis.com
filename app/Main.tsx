07c610d3b787f8ef53dcc3e059ae68aee63aaf7f
Ian <ichan@moneylion.com>
2024-06-02 03:29:30 +0800

    idk

--- app/Main.tsx
+++ app/Main.tsx
@@ -2,7 +2,11 @@ import Link from '@/components/Link'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
-import NewsletterForm from 'pliny/ui/NewsletterForm'
+import { Greeting } from '@/components/homepage/Greeting'
+import { Heading } from '@/components/homepage/Heading'
+import { ShortDescription } from '@/components/homepage/ShortDescription'
+import { TypedBios } from '@/components/homepage/TypedBios'
+import { ProfileCard } from '@/components/ProfileCard'
 
 const MAX_DISPLAY = 5
 
@@ -11,7 +15,15 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <Greeting />
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <Heading />
+            <TypedBios />
+            <ShortDescription />
+            <Heading />
+            <div className="hidden xl:block">
+              <ProfileCard />
+            </div>
             Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
