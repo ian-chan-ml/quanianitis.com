230c41d5df1d9f6ed5e4882eb5dde8f9d0809912
Ian <ichan@moneylion.com>
2024-07-22 02:07:14 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,13 +20,13 @@ export default function Footer() {
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{` • `}</div>
-          <div>{`© ${new Date().getFullYear()}`}</div>
+          <div>{` Kuala Lumpur, Malaysia `}</div>
           <div>{` • `}</div>
-          <Link href="/">{siteMetadata.title}</Link>
+          <div>{` ${new Date().getFullYear()}`}</div>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Made with Google Cloud • Cloudflare • NextJS
+          <Link href="https://github.com/ian-chan-ml/quanianitis.com">
+            Built on Google Cloud • Cloudflare • NextJS
           </Link>
         </div>
       </div>
